f7cfa9f0ad0f64844ebc0e97baff3efa1369492f
Winstonsh <winston_halim@hotmail.com>
2020-09-16 09:56:56 +1000

    restructure reducer's if to switch statement

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,37 +1,31 @@
 const initialState = {
     counter: 0
-}
+};
 
 const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT') {
-        return({
-            ...state,
-            counter: state.counter + 1
-        })
+    switch(action.type) {
+        case 'INCREMENT':
+            return({
+                ...state,
+                counter: state.counter + 1
+            });
+        case 'DECREMENT':
+            return({
+                ...state,
+                counter: state.counter - 1
+            });
+        case 'ADD':
+            return({
+                ...state,
+                counter: state.counter + action.val
+            });
+        case 'SUBTRACT':
+            return({
+                ...state,
+                counter: state.counter - action.val
+            });
     }
-
-    if(action.type === 'DECREMENT') {
-        return({
-            ...state,
-            counter: state.counter - 1
-        })
-    }
-
-    if(action.type === 'ADD') {
-        return({
-            ...state,
-            counter: state.counter + 5
-        })
-    }
-
-    if(action.type === 'SUBTRACT') {
-        return({
-            ...state,
-            counter: state.counter - 5
-        })
-    }
-
     return state;
 }
 
-export default reducer
+export default reducer;
